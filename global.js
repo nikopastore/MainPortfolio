1c0b1a9e690a5b253886f8ec75dd11910a42e53c
Niko Pastore <nikopastore@gmail.com>
2024-10-17 22:04:13 -0700

    automated navigation bar

--- global.js
+++ global.js
@@ -14,3 +14,40 @@ let currentLink = navLinks.find((a) => {
 if (currentLink) {
   currentLink.classList.add('current');
 }
+
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'cv/', title: 'CV' },
+  { url: 'https://github.com/nikopastore', title: 'GitHub Profile' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  if (!ARE_WE_HOME && !url.startsWith('http')) {
+    url = '../' + url;
+  }
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+
+  if (a.host !== location.host) {
+    a.target = '_blank';
+  }
+
+  nav.append(a);
+}
